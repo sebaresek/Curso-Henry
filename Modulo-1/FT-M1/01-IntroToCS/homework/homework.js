37ca59994c431cf4b19f97cde9b23ff3b4713ae3
resekk <sebaresekk@gmail.com>
2023-03-13 16:19:04 -0300

    seba resek

--- Modulo-1/FT-M1/01-IntroToCS/homework/homework.js
+++ Modulo-1/FT-M1/01-IntroToCS/homework/homework.js
@@ -1,8 +1,27 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let count = 0;
 
-function DecimalABinario(num) {}
+   for (i = 0; i < num.length; i++) {
+    count += (num.startsWith("1") 
+              ? num[i] * 2 ** i 
+              : num[i] * 2 ** i / 2);
+   }
+   return count;
+ }
+
+
+function DecimalABinario(num) { 
+   let almacenador = [];
+
+   while(num !== 0){
+      let residuo = parseInt(num%2)
+      num = Math.floor(num/2);
+      almacenador.push(residuo);
+   }
+   return almacenador.reverse().join('');
+}
 
 module.exports = {
    BinarioADecimal,
